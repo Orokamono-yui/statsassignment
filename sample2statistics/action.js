c610db53296d94effc8c7d5ae9209c2ce75dcfdb
Orokamono-yui <63452098+Orokamono-yui@users.noreply.github.com>
2020-05-16 20:23:59 +0900

    Add files via upload

--- sample2statistics/action.js
+++ sample2statistics/action.js
@@ -1,15 +1,29 @@
 //ウインドウの起動時・拡大縮小時に自動的にコンテンツの幅をかえる
-window.onload = widthSet; 
-window.onresize = widthSet;
+window.onload = sizeSet;
+window.onresize = sizeSet;
 
-function widthSet() { 
-	$('.image').css('height', $('#container').css('height'));
+function sizeSet() { 
+	//画面サイズ変更と同時に要素のサイズも変更
+	var allHeight = $('#container').innerHeight() + $('#main').innerHeight();	//全要素の高さ
+	var headnavHeight = $('header').outerHeight() + $('.navpanel').innerHeight();//メインコンテンツの縦の位置
+	$('.image').css('height', allHeight+'px');
 	$('.image').css('width', window.innerWidth+'px');
+	$('#main').css('top', headnavHeight+'px');
 	if(window.innerWidth > 900){
 		$('#container').css('left', (window.innerWidth-700)/2+'px');
 	}else{
 		$('#container').css('left', '100px');	//左の余白は１００以上にします
 	}
+	
+	//ナビゲーションに触ったらメニューが伸びる。他の要素に触ればなくなる。
+	$('.navhead').mouseover(function(){
+		$('.dropdown').css('display', 'none');
+			$(this).siblings('.dropdown').slideToggle(500);
+	});
+	$('#backImage3, header, #main').mouseover(function(){
+		$('.dropdown').css('display', 'none');
+	});
+
   }
 
 //１０秒間隔で背景の画像を変更する。
